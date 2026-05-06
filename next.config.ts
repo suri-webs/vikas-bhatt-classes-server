7ec84340f85449e0b0bf818a05dfd2134735125a
suri-webs <shakyasuraj595@gmail.com>
2026-05-06 13:51:40 +0530

    merge main

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
   serverExternalPackages: ["mongoose", "jsonwebtoken", "bcryptjs", "nodemailer"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
